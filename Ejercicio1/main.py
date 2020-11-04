dd4d60879f124f3e24dda6197165e72d26aec58b
PabloM <wuerbo@gmail.com>
2020-11-04 19:38:43 +0100

    added logger; import primes from list; prime number filtering from divisor calculation

--- Ejercicio1/main.py
+++ Ejercicio1/main.py
@@ -1,20 +1,22 @@
 from argparse import ArgumentParser
 import math
+import logging
+
 
 def init_args():
 
     parser = ArgumentParser(
         description='Number classifier')
-    # parser.add_argument('input_file_path',
-    #                     help='path for the input file')
+    parser.add_argument('n', type=int, help='upper limit to search upto')
     args = parser.parse_args()
-
     return args
 
-def read_numbers():
-
-    return [6, 12, 30, 2, 28, 70, 9]
-
+def get_logger(level=logging.INFO):
+    logger = logging.getLogger('perfect_numbers')
+    fh = logging.FileHandler('./classify_numbers.log', mode='w')
+    logger.addHandler(fh)
+    logger.setLevel(level)
+    return logger
 
 def get_divisors(n):
     # TODO: should check if existing divisors have been already been calculated
@@ -29,13 +31,17 @@ def get_divisors(n):
             divisor_sum += x
     return divisors, divisor_sum
 
+
 def get_divisor(n):
     yield 1
     until = math.isqrt(n) + 1 # isqrt is the floor square root
     for x in range(2, until):
+        if primes.get(str(x)):
+            continue
         if (n % x == 0): # divisor
             yield x
-            yield n//x
+            if x != n//x:
+                yield n//x
 
 # 12 = 1 + 2 + 3 + 4 + 6 ==> abundante
 # 6 = 1 + 2 + 3 ==> perfecto
@@ -43,34 +49,64 @@ def get_divisor(n):
 def get_number_type(n, _divisors):
     return 'number_type'
 
+
 def classify():
     number_list = range(n)
     for num in number_list:
         # divisors, divisor_sum = get_divisors(num)
         divisor_sum = 0
         str_divisors = ''
+        num_divisors = 0
         # import pdb; pdb.set_trace()
-        for divisor in get_divisor(num):
+        if primes.get(str(num)):
+            logger.info('****** %s ******' % num)
+            logger.info('%s is a prime number' % num)
+            continue
+
+        max_divisors = 2 * math.isqrt(num)
+        logger.info('****** %s ******' % num)
+        logger.debug('Max divisors for {} are {}'.format(num, max_divisors))
+        for i, divisor in enumerate(get_divisor(num), start=1):
             divisor_sum += divisor
             str_divisors += ' + {}'.format(str(divisor))
-        print('****** %s ******' % num)
-        print('Divisors: {}'.format(str_divisors))
+            num_divisors += 1
+            if i == max_divisors:
+                logger.debug('Early exit for number %s' % num)
+                break
+            else:
+                logger.debug('No exit found for number {0} at iterator {1} where 2*sqrt of {0} is {2}'.format(num, i, max_divisors))
+
+        if num_divisors == 1:
+            logger.debug('Only 1 divisor found, adding to primes list %s' % num)
+            primes[str(num)] = True
+
+        logger.info('Divisors: {}'.format(str_divisors))
         if divisor_sum == num:
-            print('%s is a perfect number' % num)
+            logger.info('%s is a perfect number' % num)
         elif divisor_sum > num:
-            print('%s is an abundant number' % num)
+            logger.info('%s is an abundant number' % num)
         elif divisor_sum < num:
-            print('%s is deficient number' % num)
+            logger.info('%s is deficient number' % num)
+
+
+def import_primes(filename):
+    primes = {}
+    with open(filename, 'r') as f:
+        contents = f.read()
+        for prime in contents.split('-'):
+            primes[prime] = True
+    return primes
 
 
 if __name__ == "__main__":
-    # args = init_args()
+    args = init_args()
+    global primes
+    primes = import_primes('./primes.txt')
     global n
-    n = 100000
-    #numbers = read_numbers()
+    n = args.n
+
+    global logger
+    logger = get_logger(logging.INFO)
     import timeit
-    # numbers = range(30)
-    # classify(numbers)
     exec_time = timeit.timeit(classify, number=1, globals=globals())
     print(exec_time)
-    
--- README.md
+++ README.md
@@ -1,25 +1,32 @@
+# Exercise1
 
+## Execution
 
-# Ejercicio1 optimization
+`python
+python main.py -n <int number>
+`
 
+## Optimization
 
-## Improve upper limit from n/2 to sqrt(n)
+## 1. Improve upper limit from n/2 to sqrt(n)
 After a bit of reading, we can improve the upper limit of our divisors checking. At first I went intuitively with n/2 for the worst case escenario of a number being a divisor. However, since n = a * b at the worst case, both are equal and their product can't exceed n, so, at its maximun, a divisor can be sqrt(n).
 
-Time n/2 vs sqrt(n) for the first 100000 numbers:
-153.47sec vs 14.13sec
-## Improve the number of divisors that we check
+### Time of n/2 vs sqrt(n) for the first 100000 numbers:
+- 153.47sec vs 14.13sec
+
+## 2. Improve the number of divisors that we check
 According to [this](https://math.stackexchange.com/questions/2920119/the-number-of-divisors-being-less-than-or-equal-to-twice-the-square-root-of-a-na) source we have:
 d(n) <= 2 * sqrt(n)
 
 So we can potentially shave off some iterations if we implement this check into the get_divisors function.
+However, after trying this, apparently the opportunity never presents itself to use this as an early exit for the algorithm(at least not with the numbers I've tried)
 
-
-
-## Skip known prime numbers from divisor calculation
+## 3. Skip known prime numbers from divisor calculation
 Since we have to potentially check for big numbers, upon finding a number that its prime, we don't have to check for future divisors.
-I've downloaded a few of them.
+Two things can be done to further improve here:
+    - start checking for numbers that all the way to its square root don't have any divisor to include them on a prime dictionary, so that each time they reappear on another iteration we can save some time "calculating" their primes.
+    - we could also download a list of primes and just 
 
-## Store divisors of previous calculations
-We could try to store the divisors of the numbers we check so that if a greater multiple of them is asked we can skip that section of the divisor calculation.
+## 4. Store divisors of previous calculations
+We can store the divisors of the numbers we check so that if a greater multiple of them is asked we can skip that section of the divisor calculation.
 
